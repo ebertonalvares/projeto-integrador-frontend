fda15aa1effae6e80a22ae368ec2d5f1a75f2a68
Eberton Mauricio Alvares <ebertonalvares@hotmail.com>
2024-03-08 19:34:53 -0300

    Primeiro Commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+export default function Header(){
+    return(
+         <header id="menu">
+                    <nav>
+                        <dt>
+                            <dl>
+                                <dt>Home</dt>
+                                <dt>Agendamento</dt>
+                                <dt>Contato</dt>
+                                <dt>Login</dt>
+                            </dl>
+                        </dt>
+                    </nav>
+                </header>  
+    )
+}
